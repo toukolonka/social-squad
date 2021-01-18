bd79d0cfd12ff9fe75da8f77e3b7fa968d0599f4
Your Name <you@example.com>
2021-01-18 14:11:20 +0200

    Backend bug with /me and others

--- index.js
+++ index.js
@@ -29,6 +29,10 @@ mongoose
 const app = express()
 app.use(express.static('build'))
 
+app.get('*',(req, res) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+})
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
