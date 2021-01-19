82568b654201b17bae2dd53f43d6419534ea9f25
Your Name <you@example.com>
2021-01-19 10:18:23 +0200

    fix

--- index.js
+++ index.js
@@ -28,9 +28,7 @@ mongoose
   })
 
 const app = express()
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('build'))
-}
+app.use(express.static('build'))
 
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
